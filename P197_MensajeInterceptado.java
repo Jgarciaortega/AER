0369e32d87a62e0cf2d9312874db1f21f54b9172
Jgarciaortega <46044058+Jgarciaortega@users.noreply.github.com>
2019-03-12 10:52:23 +0100

    Add files via upload

--- P197_MensajeInterceptado.java
+++ P197_MensajeInterceptado.java
@@ -28,15 +28,19 @@ public class P197_MensajeInterceptado {
 			posInicioConsonante = 0;
 			posInicioVocal = 0;
 
-			//X''
+			/*X'': Este cifrado consiste en colocar las letras en posición impar al final de la cadena.
+			Así por ejemplo, la letra que figure en la segunda posición (1 para los informáticos), deberá
+			ser la última letra de la nueva cadena. Luego seguiremos con la que esté en posición cuarta (
+			3 para los informáticos) y esta será la penultima letra. Así hasta completar todo la cadena.*/
+			
 			for(int i = 0; i < mensaje.length(); i++) {
 
-				if (i % 2 == 0) {
+				if (i % 2 == 0) { //Las que están en posición par las respetamos y las añadimos a la cadena
 
 					digito = "" + mensaje.charAt(i);
 					mensajeCifrado += digito;
 
-				}else {
+				}else { //Utilizamos una pila para añadir todas las letras que irán al final. 
 
 					digito = "" + mensaje.charAt(i);
 					pila.push(digito);
@@ -46,20 +50,23 @@ public class P197_MensajeInterceptado {
 
 			for(int i = 0; i < aux; i++) {
 
-				mensajeCifrado += pila.pop();
-
+				mensajeCifrado += pila.pop();//Cuando finalice la cadena solo tendremos que "volcar" el contenido de la pila
+											//en el mensaje Cifrado. Al ser una pila, las que entraron en primer lugar serán las
+											//primeras ha añadir.
 			}
 
-			//X'
+			/*X': El segundo cifrado consiste en intercambiar las secuencias de consonantes que localicemos. Por lo que las secuencias
+			consonante-vocal se respetan, pero cuando localicemos varias consonantes seguidas deberemos intercambiar la primera de ellas
+			por la última de la secuencia, luego, si hay más, sería la segunda por la penúltima, así hasta finalizar la secuencia.*/
 			aux = 0;
 
 			for(int i = 0; i < mensajeCifrado.length(); i++) {
 
-				digito = "" + mensajeCifrado.charAt(i);
+				digito = "" + mensajeCifrado.charAt(i); //Recogemos el digito en formato String para añadirlo en la cadena posteriormente
 
-				if (comprobarDigito (mensajeCifrado.charAt(i))) {
-
-					for (int j = 0; j < aux && aux > 0; j++) {
+				if (comprobarDigito (mensajeCifrado.charAt(i))) { //Si es vocal entramos en esta condición. Si hay contenido en la pila
+																//lo añadirá al mensaje cifrado, si no lo hay directamente añadirá la vocal
+					for (int j = 0; j < aux && aux > 0; j++) {  //a la secuencia.
 
 						mensajeCifrado2 += pila.pop();
 
@@ -68,15 +75,16 @@ public class P197_MensajeInterceptado {
 					mensajeCifrado2 += digito;
 
 
-				}else {
-
+				}else {	//Cuando no localiza vocal añadirá a la pila las consonantes, si solo hay una en la siguiente vocal volcará el contenido
+						//en el condicional de arriba, si vuelve a haber otra consonante la añadira a la pila. 
 					pila.push(digito);
 					aux = pila.size();
 				}				
 
 			}
 
-			if(aux > 0) {
+			if(aux > 0) { //Puede darse la circumnstancia de que no haya volcado todo, ya que si no localiza vocal no añade el contenido de la pila.
+						 //Por esa razon creo que este condicional, el cual me dice si hay contenido en la pila, añadelo a mensaje cifrado
 
 				for(int j = 0; j < aux; j++) {
 
